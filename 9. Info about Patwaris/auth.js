c06cd079261044e28809d0d8db0dc2d49ae2fe49
juanaamirhussain <jaunaamirhussain@gmail.com>
2025-01-15 23:50:51 +0500

    Delete 9. Info about Patwaris/auth.js

--- 9. Info about Patwaris/auth.js
+++
@@ -1,9 +0,0 @@
-function authenticate() {
-    let code = prompt('Enter the access code:');
-    if (code !== '7897836') {
-        alert('Incorrect code! Access denied.');
-        window.location.href = 'index.html'; // Redirect to the form page
-    } else {
-        loadData(); // Load the data if the code is correct
-    }
-}
